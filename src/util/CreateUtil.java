5671ccc82e653bcd65f288a0e02ae6dce2288db1
GCXMB-GFT <GCXMB-GFT@DESKTOP-ECLIPSE>
2018-09-17 16:32:54 +0800

    randomNum

--- src/util/CreateUtil.java
+++ src/util/CreateUtil.java
@@ -1,5 +1,13 @@
 package util;
 
 public class CreateUtil {
-	
+	public static int randomValue(int rA,int rB) {
+		int range=rB-rA;
+		return (int)Math.rint(range*Math.random()+rA);	
+	}
+	public static void main(String args[]) {
+		for (int i=0;i<20;i++) {
+		System.out.print(randomValue(0,10)+" ");
+		}
+	}
 }
