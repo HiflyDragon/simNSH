68712b56c49e30f99c1c89e1783c724e7980e407
GCXMB-GFT <GCXMB-GFT@DESKTOP-ECLIPSE>
2018-09-25 07:47:49 +0800

    Data

---
+++ src/util/Data.java
@@ -0,0 +1,13 @@
+package util;
+
+public class Data {
+	public static String NameRange[] = new String[] { "", "蔽月回雪缎" };
+	public static String QualityRange[] = new String[] { "蓝及以上", "红及以上", "蓝", "红", "紫" };
+	public static String PartRange[] = new String[] { "随机", "武器", "项链", "手镯", "戒指", "头部", "上衣", "腕部", "手套", "腰带",
+			"鞋子" };
+	public static String SectRange[] = new String[] { "随机", "血河", "铁衣", "神相", "九灵", "素问", "碎梦" };
+	public static String LevelRange[] = new String[] { "60~69" };
+	public static String suwen60Block1[]= new String[] {"随机","刘涓子·霜天护腕" };
+	public static String suwen60Block2[]= new String[] {"随机","刘涓子·碧牙绸缎" };
+	public static String suwen60Block3[]= new String[] {"随机","刘涓子·碧牙坠环" };
+}
